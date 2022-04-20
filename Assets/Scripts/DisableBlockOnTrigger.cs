42209b6313c9beea9edc922ec87bca5994f678c6
nicolas catanoso <nicos445@hotmail.fr>
2022-04-20 19:20:38 +0200

    [FIX] z fight

--- Assets/Scripts/DisableBlockOnTrigger.cs
+++ Assets/Scripts/DisableBlockOnTrigger.cs
@@ -3,8 +3,10 @@
 public class DisableBlockOnTrigger : MonoBehaviour
 {
     [SerializeField] private Transform spawnPosition;
+    [SerializeField] private Transform lastBlock;
     public void Disable(Collider other)
     {
-        other.transform.position = spawnPosition.position;
+        other.transform.position = lastBlock.position + Vector3.forward * lastBlock.localScale.z * 1f;
+        lastBlock = other.transform;
     }
 }
